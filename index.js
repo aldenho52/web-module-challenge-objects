f737799c4a23d8571f62198b23128951499f56ef
Alden Ho <aldenho@Aldens-MacBook-Air.local>
2020-09-10 13:23:03 -0700

    completed stretch 1

--- index.js
+++ index.js
@@ -135,10 +135,16 @@ function getLastReview(array) {
     {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."}]
 */
 
- function getReviewByRating(/* code here */) {
-    /* code here */
-  }
+ function getReviewByRating(array, rating) {
+  let listOfRatings = []  
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].rating === Math.floor(rating)) {
+      listOfRatings.push(array[i])
+    } 
+  } return listOfRatings
+}
 
+console.log(getReviewByRating(reviews, 4))
   
 /** STRETCH 2: Write a function called 'getLongestReview' that returns an array containing all reviews longer than 15 words. 
   
